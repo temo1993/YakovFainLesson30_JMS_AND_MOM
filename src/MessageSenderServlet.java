10989eb48418d15c4f7472acaa9c2272816618e5
Temo Kiknavelidze <temo.kiknavelidze@gmail.com>
2016-03-21 18:11:16 +0400

    ...

--- src/MessageSenderServlet.java
+++ src/MessageSenderServlet.java
@@ -17,7 +17,7 @@ public class MessageSenderServlet extends HttpServlet {
     @Resource(lookup ="java:comp/DefaultJMSConnectionFactory")  // JNDI name
             ConnectionFactory factory;
 
-    @Resource(lookup = "OutGoingTradeOrders")  // JNDI name
+    @Resource(lookup = "jms/OutGoingTradeOrders")  // JNDI name
             Destination ordersQueue;
 
 
